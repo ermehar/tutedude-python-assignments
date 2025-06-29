5436fd2094eaa25936375227d8ea952aaf9d7fdb
Yashwant Mehar <yashwantmehar@gmail.com>
2025-06-29 23:04:34 +0530

    Adding Files for Assignment-1 Resubmition

---
+++ Assignment-1-Resubmision/task-1.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(f"Addition: {num1} + {num2} = {addition}")
+print(f"Subtraction: {num1} - {num2} = {subtraction}")
+print(f"Multiplication: {num1} × {num2} = {multiplication}")
+print(f"Division: {num1} ÷ {num2} = {division}")
